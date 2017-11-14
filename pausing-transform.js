1a812d8ea958cb06625aaf1608f3c11a7cc865e5
Dan Kolz <dan@emergentideas.com>
2017-11-14 07:25:04 -0600

    buffers until flush or end

---
+++ pausing-transform.js
@@ -0,0 +1,39 @@
+const { Transform, Writeable } = require('stream');
+
+class PausingTransform extends Transform {
+	constructor(transformer) {
+		super()
+		this.pausedData = ''
+		this.paused = true
+		this.transformer = transformer
+	}
+	_transform(chunk, encoding, callback) {
+		
+		return callback(null, this.transformer ? this.transformer(chunk.toString()) : chunk)
+	}
+	_flush(callback) {
+		let ret = super.write(this.pausedData, null, callback)
+		this.pausedData = ''
+		return ret
+	}
+	write(chunk, encoding, callback) {
+		if(this.paused) {
+			this.pausedData += chunk.toString()
+			if(callback) {
+				callback()
+			}
+		}
+		else {
+			return super.write(chunk, encoding, callback)
+		}
+	}
+	pause() {
+		this.paused = true
+	}
+	run() {
+		this.paused = false
+		this._flush()
+	}
+}
+
+module.exports = PausingTransform
